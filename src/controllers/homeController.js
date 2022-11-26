ba2f53e0d313a2cbec81bb54a79a729983c61efc
TeodorLevakov <37295419+TeodorLevakov@users.noreply.github.com>
2022-11-26 17:46:23 +0100

    add new rubic
    Add persistance

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,20 @@
+const router = require('express').Router();
 const cubes = require('../db.json');
 
-exports.index = (req, res) => {
-    res.render('index', { cubes });
-};
 
-exports.about = (req, res) => {
+router.get('/', (req, res) => {
+    res.render('index', { cubes });
+});
+
+router.get('/about', (req, res) => {
     res.render('about');
-};
+});
+
+module.exports = router;
+// exports.index = (req, res) => {
+//     res.render('index', { cubes });
+// };
+
+// exports.about = (req, res) => {
+//     res.render('about');
+// };
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ const cubeController = require('./controllers/cubeController.js');
 
 const router = express.Router();
 
-router.get('/', homeController.index);
-router.get('/about', homeController.about);
+router.use('/', homeController);
+//router.get('/about', homeController.about);
 
 router.use('/cube', cubeController);
 
